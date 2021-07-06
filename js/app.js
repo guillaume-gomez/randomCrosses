3ddb4bf499347c70465df33b124f9c2c52ddddff
Guillaume Gomez <guillaume.gomez846@gmail.com>
2021-07-06 19:09:21 +0200

    write a naive approach

--- js/app.js
+++ js/app.js
@@ -1,9 +1,47 @@
-function resize(){    
-  $("#canvas").outerHeight($(window).height()-$("#canvas").offset().top- Math.abs($("#canvas").outerHeight(true) - $("#canvas").outerHeight()));
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
 }
-$(document).ready(function(){
-  resize();
-  $(window).on("resize", function(){                      
-      resize();
-  });
+function setBackgroundColor(canvas) {
+    var ctx = getContext(canvas);
+    ctx.fillStyle = getRandomColor();
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+}
+function getCanvas() {
+    var canvas = document.getElementById("canvas");
+    if (!canvas) {
+        throw "cannot find the canvas in the page";
+    }
+    return canvas;
+}
+function getContext(canvas) {
+    var context = canvas.getContext("2d");
+    if (!context) {
+        throw "cannot find the context in the page";
+    }
+    return context;
+}
+function createCross(context, x, y) {
+    var widthCross = 10;
+    context.beginPath();
+    context.moveTo(x, y + widthCross / 2);
+    context.lineTo(x + widthCross, y + widthCross / 2);
+    context.moveTo(x + widthCross / 2, y);
+    context.lineTo(x + widthCross / 2, y + widthCross);
+    context.stroke();
+}
+window.addEventListener("load", function (event) {
+    var canvas = getCanvas();
+    var context = getContext(canvas);
+    setBackgroundColor(canvas);
+    for (var i = 0; i < 50; i++) {
+        var x = Math.random() * canvas.width;
+        var y = Math.random() * canvas.height;
+        createCross(context, x, y);
+    }
 });
+//# sourceMappingURL=app.js.map
--- js/app.ts
+++ js/app.ts
@@ -8,20 +8,47 @@ function getRandomColor() : string {
 }
 
 
-function setBackgroundColor() : void {
-  let canvas = document.getElementById("canvas") as HTMLCanvasElement;
-  if(!canvas) {
-    return;
-  }
-
-  let ctx = canvas.getContext("2d");
-  if(!ctx) {
-    return
-  }
+function setBackgroundColor(canvas: HTMLCanvasElement) : void {
+  let ctx = getContext(canvas);
   ctx.fillStyle = getRandomColor();
   ctx.fillRect(0, 0, canvas.width, canvas.height);
 }
 
+function getCanvas() : HTMLCanvasElement {
+  let canvas = document.getElementById("canvas") as HTMLCanvasElement;
+  if(!canvas) {
+    throw "cannot find the canvas in the page";
+  }
+  return canvas;
+}
+
+function getContext(canvas: HTMLCanvasElement) : CanvasRenderingContext2D {
+  let context = canvas.getContext("2d");
+  if(!context) {
+    throw "cannot find the context in the page";
+  }
+  return context;
+}
+
+function createCross(context: CanvasRenderingContext2D, x: number, y: number) : void {
+  const widthCross = 10;
+  context.beginPath();
+  context.moveTo(x,y + widthCross/2);
+  context.lineTo(x + widthCross, y + widthCross/2);
+
+  context.moveTo(x + widthCross/2 ,y);
+  context.lineTo(x + widthCross/2, y + widthCross);
+
+  context.stroke();
+}
+
 window.addEventListener("load", function(event) {
-  setBackgroundColor();
+  let canvas = getCanvas();
+  let context = getContext(canvas);
+  setBackgroundColor(canvas);
+  for(let i = 0; i < 50; i++) {
+    const x = Math.random() * canvas.width;
+    const y = Math.random() * canvas.height;
+    createCross(context, x, y);
+  }
 });
